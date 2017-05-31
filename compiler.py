242c9eb5030cf6021fe427d1e82af0846f60fa74
Mark Lavrentyev <lavrema@outlook.com>
2017-05-31 15:35:26 -0400

    Write loop method

--- compiler.py
+++ compiler.py
@@ -1,40 +1,91 @@
 import re
 
-class BFcompiler(object):
+class BFCompiler(object):
 
     def __init__(self):
         # Initialize tape and dataPointer
         self.dataPointer = 0
-        self.tape = [0 for x in range(30000)]
+        self.instPointer = 0
+        self.tape = [0 for x in range(20)]
         self.errors = []
         
         # Errors:
         #   -1: Invalid program character
         #   -2: Pointer out of bounds
         #   -3: Invalid input character
+        #   -4: Invalid value
 
-    def parseBF(sourceCode):
+    def parseBF(self, sourceCode):
         # Remove comments
         sourceCode = re.sub(r'[^\[\]+-<>.,]*', "", sourceCode)
-
+        self.sourceCode = sourceCode
         
+        while self.instPointer < len(sourceCode):
+            char = self.sourceCode[self.instPointer]
+            if char == '>' or char == '<':
+                self.pointerMovement(char)
+                self.instPointer += 1
+            elif char == '+' or char == '-':
+                self.changeValue(char)
+                self.instPointer += 1
+            elif char == '.':
+                self.showOutput()
+                self.instPointer += 1
+            elif char == ',':
+                self.takeInput()
+                self.instPointer += 1
+            elif char == '[':
+                self.instPointer += 1
+                self.execLoop()
+            else:
+                self.errors.append(-1)
 
-    def pointerMovement(command):
+    def execLoop(self):
+        while self.sourceCode[self.instPointer] != ']':
+            char = self.sourceCode[self.instPointer]
+            if char == '>' or char == '<':
+                self.pointerMovement(char)
+                self.instPointer += 1
+            elif char == '+' or char == '-':
+                self.changeValue(char)
+                self.instPointer += 1
+            elif char == '.':
+                self.showOutput()
+                self.instPointer += 1
+            elif char == ',':
+                self.takeInput()
+                self.instPointer += 1
+            elif char == '[':
+                self.instPointer += 1
+                self.execLoop()
+            else:
+                self.errors.append(-1)
+        self.instPointer += 1
+
+    def pointerMovement(self, command):
         # command - string, either < or >
         if command == '>':
             self.dataPointer += 1
         elif command == '<':
             self.dataPointer -= 1
-        else:
-            self.errors.append(-1)
 
         if self.dataPointer < 0 or self.dataPointer > len(self.tape):
             self.errors.append(-2)
 
-    def showOutput():
+    def changeValue(self, command):
+        # command - string, either + or -
+        if command == '+':
+            self.tape[self.dataPointer] += 1
+        elif command == '-':
+            self.tape[self.dataPointer] -= 1
+
+        if self.tape[self.dataPointer] < 0:
+            self.errors.append(-4)
+
+    def showOutput(self):
         print(chr(self.tape[self.dataPointer]))
 
-    def takeInput():
+    def takeInput(self):
         inVal = input("Input: ")
         value = inVal[0]
 
@@ -45,10 +96,7 @@ class BFcompiler(object):
     
 
 
-sourceCode = """
-Hello this is BF
->>.++[-abc]<<
-dooggo
-> helloo"""
+sourceCode = """+++++++++[>++++++++++<-]>."""
 
-parseBF(sourceCode)
+bfc = BFCompiler()
+bfc.parseBF(sourceCode)
